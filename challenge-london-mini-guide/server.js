2af19f07e4a4d4d84794479b1c5d7d9970d7af4a
Farhana <farhanafatema@hotmail.com>
2020-09-05 16:20:49 +0100

    working on level 200

--- challenge-london-mini-guide/server.js
+++ challenge-london-mini-guide/server.js
@@ -4,18 +4,67 @@ const harrow = require ('./data/Harrow.json');
 const heathrow = require('./data/Heathrow.json');
 const startford = require('./data/Stratford.json');
 
-app.get('/harrow/pharmacies', function (req, res) {
+// Harrow endpoint using params
+
+app.get('/harrow/:service', function (req, res) {
+    const service = req.params.service;
+    switch(service){
+        case "pharmacies":
+            const pharmacies = harrow.pharmacies;
+            if(pharmacies){
+                res.json(pharmacies);
+            } else{
+                res.status(404).send("Not Found");
+            }
+        break;
+
+        case "colleges":
+            const colleges = harrow.colleges;
+            if(colleges){
+                res.json(colleges);
+            } else{
+                res.status(404).send("Not Found");
+            }
+            break;
+        case "doctors":
+            const doctors = harrow.doctors;
+            if(doctors){
+                res.json(doctors);
+            } else{
+                res.status(404).send("Not Found");
+            }  
+            break;
+        case "hospitals":
+            const hospitals = harrow.hospitals;
+            if(hospitals){
+                res.json(hospitals);
+            } else{
+                res.status(404).send("Not Found");
+            }
+            break;
+        default:
+            res.status(404).send("Not Found");
+            break;    
 
 
-    const pharmacies = harrow.pharmacies;
-    if(pharmacies !== undefined){
-        res.json(pharmacies);
-    } else{
-        res.status(404).send("Not Found");
+            
     }
+
+
+        
+    
+
+    if(service === 'hospitals'){
+      
+
+    }
+           
+
     
     
-  })
+    
+  });
+
 
   app.get('/heathrow/pharmacies', function (req, res) {
 
